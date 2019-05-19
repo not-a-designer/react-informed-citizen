9d0f13f60840fcd367e6b46b2a27f27959caa410
not-a-designer <joel.johnson.design@gmail.com>
2019-05-18 23:27:22 -0500

    axios added, info windows added

--- src/components/MapComponent/MapComponent.tsx
+++ src/components/MapComponent/MapComponent.tsx
@@ -1,26 +1,77 @@
 import React, { Component, RefObject, createRef } from 'react';
-import { withGoogleMap, withScriptjs, GoogleMap } from 'react-google-maps';
+import { withGoogleMap, withScriptjs, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
+import InfoBox from 'react-google-maps/lib/components/addons/InfoBox';
+import { IonSpinner } from '@ionic/react';
 
 
 class MapComponent extends Component<any, any> {
 
+    state = {
+        selectedMarker: null
+    }
+
+    markerClickHandler = (event: any, index: any) => {
+        // close all info windows
+        this.props.markers.forEach((marker: any) => this.closeMarkerHandler(marker));
+        const selectedMarker = this.props.markers[index];
+        //open specific info window
+        selectedMarker.isOpened = true;
+        this.setState({ selectedMarker: selectedMarker });
+    }
+
+    closeMarkerHandler = (marker: any) => {
+        marker.isOpened = false;
+    }
+
     render() {
         //console.log(this.props);
-        const { center, zoom } = this.props;
+        const { center, zoom, markers } = this.props;
 
         const defaultOpts = {
             disableDefaultUI: true
         };
-
+        let markerList: any[] = [];
+        if (markers.length) {
+            markerList = markers.map((marker: any, i: number) => {
+                let key: string;
+                if (marker['ADDRESS']) {
+                    key = marker.ADDRESS;
+                }
+                else if (marker['ACCIDENTLOC']) {
+                    key = marker.ACCIDENTLOC;
+                }
+                else if (marker['Polling Place Address']) {
+                    key = marker._id;
+                }
+                else {
+                    key = marker.IncidentNum;
+                }
+                return (
+                    <Marker key={key} position={marker.position} onClick={(event) => this.markerClickHandler(event, i)}>
+                        {marker.isOpened && 
+                            <InfoWindow defaultPosition={marker.position} onCloseClick={() => this.closeMarkerHandler(marker)}>
+                                <p>
+                                    {marker.ADDRESS ? marker.ADDRESS : null}
+                                    {marker.ACCIDENTLOC ? marker.ACCIDENTLOC : null}
+                                    {marker['Polling Place Address'] ? marker['Polling Place Address'] : null}
+                                    {marker.Location ? marker.Location : null}
+                                </p>
+                            </InfoWindow>
+                        }
+                    </Marker>
+                );
+            })
+        }
         
-
         return (
             <GoogleMap 
                 defaultOptions={defaultOpts}
                 defaultCenter={center}
                 defaultZoom={zoom}
-                defaultClickableIcons={false} />
-        );
+                defaultClickableIcons={false}>
+                {markerList.length ? markerList : null}
+            </GoogleMap>
+        )
     }
 }
 
--- src/pages/MapPage/MapPage.tsx
+++ src/pages/MapPage/MapPage.tsx
@@ -11,9 +11,13 @@ import { IonHeader,
          IonIcon, 
          IonLabel, 
          IonSearchbar,
-         IonButton} from '@ionic/react';
+         IonButton,
+         IonSpinner} from '@ionic/react';
+
+import axios from 'axios';
 
 import MapComponent from '../../components/MapComponent/MapComponent';
+import classes from './MapPage.module.css';
 
 
 class MapPage extends Component {
@@ -21,32 +25,96 @@ class MapPage extends Component {
         {
             icon: 'warning',
             label: 'Crime',
-            value: 'crime'
+            value: 'crime',
+            id: '87843297-a6fa-46d4-ba5d-cb342fb2d3bb'
         }, {
             icon: 'stats',
             label: 'Polls',
-            value: 'polls'
+            value: 'polls',
+            id: 'b50aed9e-2893-483c-a56a-3c51530c2cc9'
         }, {
             icon: 'bicycle',
             label: 'Biking',
-            value: 'traffic'
+            value: 'traffic',
+            id: '8fffaa3a-b500-4561-8898-78a424bdacee'
         }, {
             icon: 'restaurant',
             label: 'Food',
-            value: 'food'
+            value: 'food',
+            id: 'e2fecdb9-3de9-4d34-955e-17d2e815bad6'
         }
     ];
 
     state = {
         defaultCenter: { lat: 43.09, lng: -87.9},
         defaultZoom: 13,
-        currentView: 'crime'
+        currentView: 'crime',
+        markers: [],
+        isLoading: false
     };
 
 
     changeViewHandler = (event: any, newView: string) => {
         console.log(event);
-        this.setState({ currentView: newView })
+        this.setState({ currentView: newView });
+        this.loadData();
+    }
+
+    componentDidMount() {
+        this.loadData();
+    }
+
+    async loadData() {
+        this.setState({ isLoading: true })
+        const BASE_URL = 'https://data.milwaukee.gov/api/3/action/datastore_search';
+        const currentMode = this.segmentButtons.filter((buttonData: any) => buttonData.value === this.state.currentView);
+
+        const resourceId = currentMode[0].id;
+        const response = await axios.get(BASE_URL, { params: { 'resource_id': resourceId, 'limit': 5 } })
+        //console.log(response.data);
+        let newList: any[] = [];
+        for (let record of response.data.result.records) {
+            const geocodeResponse = await this.geocodeAddress(record);
+            const newRecord = {...record, isOpened: false, position: { lat: +geocodeResponse.lat, lng: +geocodeResponse.lng } };
+            console.log('updatedMarker', newRecord);
+            newList.push(newRecord);
+        }
+
+        this.setState({ markers: newList, isLoading: false });  
+    }
+
+    async geocodeAddress(record: any) {
+        let addr: string = '';
+        let zip: number = -1;
+
+        if (record['Polling Place Address']) {
+            console.log('Polls');
+            addr = record['Polling Place Address'];
+        }
+        else if (record['ACCIDENTLOC']) {
+            console.log('Traffic');
+            addr = record.ACCIDENTLOC;
+        }
+        else if (record['ADDRESS']) {
+            console.log('Food');
+            addr = record.ADDRESS;
+        }
+        else {
+            console.log('Crime');
+            addr = record.Location;
+            zip = +record.ZIP;
+        }
+        const query = (zip < 0) ? `${addr}, Milwaukee, WI` : `${addr}, Milwaukee, WI ${zip}`
+        if (addr) {
+            const geocodeURL: string = 'https://api.opencagedata.com/geocode/v1/json';
+            const params = {
+                'key': '9df276e154574e268bca6cf24e1ade88',
+                'q': query
+            };
+            const geoResponse = await axios.get(geocodeURL, {params: params});
+            return geoResponse.data.results[0].geometry;
+        }
+        
     }
 
     render() {
@@ -81,14 +149,20 @@ class MapPage extends Component {
                         </IonSegment>
                     </IonToolbar>
                 </IonHeader>
+
                 <IonContent>
-                    <MapComponent 
-                        googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyCG7jwYwhPvLKDRESqjFcwT4-W4jSSzw4c&libraries=places`}
-                        loadingElement={<div style={{height: '100%'}} />}
-                        mapElement={<div style={{height: '100%'}} />}
-                        containerElement={<div style={{height: '100%'}} />}
-                        center={this.state.defaultCenter} 
-                        zoom={this.state.defaultZoom} />
+                    {!this.state.isLoading ? (
+                        <MapComponent 
+                            googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyCG7jwYwhPvLKDRESqjFcwT4-W4jSSzw4c&libraries=places`}
+                            loadingElement={<div style={{height: '100%'}} />}
+                            mapElement={<div style={{height: '100%'}} />}
+                            containerElement={<div style={{height: '100%'}} />}
+                            center={this.state.defaultCenter} 
+                            zoom={this.state.defaultZoom} 
+                            markers={this.state.markers} />
+                    ) : (
+                        <IonSpinner class={classes.IonSpinner} name="circles" />
+                    )}
                 </IonContent>
             </IonPage>
         )
